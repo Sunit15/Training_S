2b1ca655c40f0332acdcf2cf3a86a61ce65ec1a5
unknown <titima@pbru.ac.th>
2015-08-18 13:47:14 +0700

    after lunch

--- app/src/main/java/pbru/b/sunday/thailandtraffic/MainActivity.java
+++ app/src/main/java/pbru/b/sunday/thailandtraffic/MainActivity.java
@@ -50,6 +50,16 @@ public class MainActivity extends AppCompatActivity {
         strTitle[18] = "จำกัดความกว้าง";
         strTitle[19] = "จำกัดความสูง 5.0M";
 
+        int[] intImage = {R.drawable.traffic_01, R.drawable.traffic_02,
+                R.drawable.traffic_03, R.drawable.traffic_04, R.drawable.traffic_05,
+                R.drawable.traffic_06, R.drawable.traffic_07, R.drawable.traffic_08,
+                R.drawable.traffic_09, R.drawable.traffic_10, R.drawable.traffic_11,
+                R.drawable.traffic_12, R.drawable.traffic_13, R.drawable.traffic_14,
+                R.drawable.traffic_15, R.drawable.traffic_16, R.drawable.traffic_17,
+                R.drawable.traffic_18, R.drawable.traffic_19, R.drawable.traffic_20};
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, intImage, strTitle);
+        trafficListView.setAdapter(objMyAdapter);
+
     } // createListView
 
     private void initialWidget() {
