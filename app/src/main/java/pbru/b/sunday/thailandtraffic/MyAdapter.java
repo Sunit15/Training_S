7ca7e3f3995995fcb545fde95dba260cd051a46b
unknown <titima@pbru.ac.th>
2015-08-18 11:22:18 +0700

    Before Break

---
+++ app/src/main/java/pbru/b/sunday/thailandtraffic/MyAdapter.java
@@ -0,0 +1,46 @@
+package pbru.b.sunday.thailandtraffic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Lab on 8/18/2015.
+ */
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context objContext;
+    private int[] iconInts; //แสดงตน รูปภาพที่จะ show
+    private String[] titleStrings;
+
+    public MyAdapter(Context objContext, int[] iconInts, String[] titleStrings) {
+        this.objContext = objContext;
+        this.iconInts = iconInts;
+        this.titleStrings = titleStrings;
+    } // Constructor
+
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+      
+        return null;
+    } //getView
+} // Main Class
